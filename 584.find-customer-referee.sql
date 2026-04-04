9ed8caaf8d3c430c0a6254507084d758faa3a324
Gomathy Priyadharshini <priyaarunbalaji01@example.com>
2026-04-04 21:23:09 +0200

    Find Customer Referee

---
+++ 584.find-customer-referee.sql
@@ -0,0 +1,80 @@
+--
+-- @lc app=leetcode id=584 lang=mysql
+--
+-- [584] Find Customer Referee
+--
+-- https://leetcode.com/problems/find-customer-referee/description/
+--
+-- database
+-- Easy (72.66%)
+-- Likes:    2955
+-- Dislikes: 412
+-- Total Accepted:    2.1M
+-- Total Submissions: 2.8M
+-- Testcase Example:  '{"headers":{"Customer":["id","name","referee_id"]},"rows":{"Customer":[[1,"Will",null],[2,"Jane",null],[3,"Alex",2],[4,"Bill",null],[5,"Zack",1],[6,"Mark",2]]}}'
+--
+-- Table: Customer
+-- 
+-- 
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | referee_id  | int     |
+-- +-------------+---------+
+-- In SQL, id is the primary key column for this table.
+-- Each row of this table indicates the id of a customer, their name, and the
+-- id of the customer who referred them.
+-- 
+-- 
+-- 
+-- 
+-- Find the names of the customer that are either:
+-- 
+-- 
+-- referred by any customer with id != 2.
+-- not referred by any customer.
+-- 
+-- 
+-- Return the result table in any order.
+-- 
+-- The result format is in the following example.
+-- 
+-- 
+-- Example 1:
+-- 
+-- 
+-- Input: 
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
+-- 
+-- 
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+select name from Customer
+where ifNull(referee_id,0)!=2
+
+-- @lc code=end
+
