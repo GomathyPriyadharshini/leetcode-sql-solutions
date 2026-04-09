faaa6526b31762c5a8510cb52a98d81d75d43eb2
Gomathy Priyadharshini <priyaarunbalaji01@example.com>
2026-04-09 09:42:00 +0200

    Grouping & Join Aggregation

---
+++ 1045.customers-who-bought-all-products.sql
@@ -0,0 +1,107 @@
+--
+-- @lc app=leetcode id=1045 lang=mysql
+--
+-- [1045] Customers Who Bought All Products
+--
+-- https://leetcode.com/problems/customers-who-bought-all-products/description/
+--
+-- database
+-- Medium (63.87%)
+-- Likes:    1154
+-- Dislikes: 98
+-- Total Accepted:    477.4K
+-- Total Submissions: 747.4K
+-- Testcase Example:  '{"headers":{"Customer":["customer_id","product_key"],"Product":["product_key"]},"rows":{"Customer":[[1,5],[2,6],[3,5],[3,6],[1,6]],"Product":[[5],[6]]}}'
+--
+-- Table: Customer
+-- 
+-- 
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | customer_id | int     |
+-- | product_key | int     |
+-- +-------------+---------+
+-- This table may contain duplicates rows. 
+-- customer_id is not NULL.
+-- product_key is a foreign key (reference column) to Product table.
+-- 
+-- 
+-- 
+-- 
+-- Table: Product
+-- 
+-- 
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | product_key | int     |
+-- +-------------+---------+
+-- product_key is the primary key (column with unique values) for this
+-- table.
+-- 
+-- 
+-- 
+-- 
+-- Write a solution to report the customer ids from the Customer table that
+-- bought all the products in the Product table.
+-- 
+-- Return the result table in any order.
+-- 
+-- The result format is in the following example.
+-- 
+-- 
+-- Example 1:
+-- 
+-- 
+-- Input: 
+-- Customer table:
+-- +-------------+-------------+
+-- | customer_id | product_key |
+-- +-------------+-------------+
+-- | 1           | 5           |
+-- | 2           | 6           |
+-- | 3           | 5           |
+-- | 3           | 6           |
+-- | 1           | 6           |
+-- +-------------+-------------+
+-- Product table:
+-- +-------------+
+-- | product_key |
+-- +-------------+
+-- | 5           |
+-- | 6           |
+-- +-------------+
+-- Output: 
+-- +-------------+
+-- | customer_id |
+-- +-------------+
+-- | 1           |
+-- | 3           |
+-- +-------------+
+-- Explanation: 
+-- The customers who bought all the products (5 and 6) are customers with IDs 1
+-- and 3.
+-- 
+-- 
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+select customer_id from customer c
+group by customer_id 
+having not exists
+(
+    select 1 from product p 
+    where not exists
+    (
+        select 1 from customer c1
+        where c1.customer_id=c.customer_id
+        and c1.product_key=p.product_key
+
+    )
+)
+
+-- @lc code=end
+
