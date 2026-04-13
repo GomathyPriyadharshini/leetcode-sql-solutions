7b11e03cb7845f000ff7fa2f7b8e29dcc2775b2f
Gomathy Priyadharshini <priyaarunbalaji01@example.com>
2026-04-13 18:40:04 +0200

    Window Functions & Ranking

---
+++ 184.department-highest-salary.sql
@@ -0,0 +1,107 @@
+--
+-- @lc app=leetcode id=184 lang=mysql
+--
+-- [184] Department Highest Salary
+--
+-- https://leetcode.com/problems/department-highest-salary/description/
+--
+-- database
+-- Medium (57.70%)
+-- Likes:    2401
+-- Dislikes: 211
+-- Total Accepted:    663K
+-- Total Submissions: 1.1M
+-- Testcase Example:  '{"headers": {"Employee": ["id", "name", "salary", "departmentId"], "Department": ["id", "name"]}, "rows": {"Employee": [[1, "Joe", 70000, 1], [2, "Jim", 90000, 1], [3, "Henry", 80000, 2], [4, "Sam", 60000, 2], [5, "Max", 90000, 1]], "Department": [[1, "IT"], [2, "Sales"]]}}'
+--
+-- Table: Employee
+-- 
+-- 
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | id           | int     |
+-- | name         | varchar |
+-- | salary       | int     |
+-- | departmentId | int     |
+-- +--------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- departmentId is a foreign key (reference columns) of the ID from the
+-- Department table.
+-- Each row of this table indicates the ID, name, and salary of an employee. It
+-- also contains the ID of their department.
+-- 
+-- 
+-- 
+-- 
+-- Table: Department
+-- 
+-- 
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table. It is
+-- guaranteed that department name is not NULL.
+-- Each row of this table indicates the ID of a department and its name.
+-- 
+-- 
+-- 
+-- 
+-- Write a solution to find employees who have the highest salary in each of
+-- the departments.
+-- 
+-- Return the result table in any order.
+-- 
+-- The result format is in the following example.
+-- 
+-- 
+-- Example 1:
+-- 
+-- 
+-- Input: 
+-- Employee table:
+-- +----+-------+--------+--------------+
+-- | id | name  | salary | departmentId |
+-- +----+-------+--------+--------------+
+-- | 1  | Joe   | 70000  | 1            |
+-- | 2  | Jim   | 90000  | 1            |
+-- | 3  | Henry | 80000  | 2            |
+-- | 4  | Sam   | 60000  | 2            |
+-- | 5  | Max   | 90000  | 1            |
+-- +----+-------+--------+--------------+
+-- Department table:
+-- +----+-------+
+-- | id | name  |
+-- +----+-------+
+-- | 1  | IT    |
+-- | 2  | Sales |
+-- +----+-------+
+-- Output: 
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Jim      | 90000  |
+-- | Sales      | Henry    | 80000  |
+-- | IT         | Max      | 90000  |
+-- +------------+----------+--------+
+-- Explanation: Max and Jim both have the highest salary in the IT department
+-- and Henry has the highest salary in the Sales department.
+-- 
+-- 
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+
+select Department,Employee,Salary from (select d.name as Department,E.name as Employee,
+rank() over (partition by d.name order by salary desc) as rank_value ,salary 
+from Employee E
+inner join department d on
+d.id=E.departmentId)b
+where rank_value=1
+
+-- @lc code=end
+
