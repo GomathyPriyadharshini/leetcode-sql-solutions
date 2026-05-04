f7c6e38cf7077f900664055ff72e8b9cceb7c391
Gomathy Priyadharshini <priyaarunbalaji01@example.com>
2026-05-04 16:40:33 +0200

    New File

---
+++ 1907.count-salary-categories.sql
@@ -0,0 +1,95 @@
+--
+-- @lc app=leetcode id=1907 lang=mysql
+--
+-- [1907] Count Salary Categories
+--
+-- https://leetcode.com/problems/count-salary-categories/description/
+--
+-- database
+-- Medium (64.24%)
+-- Likes:    746
+-- Dislikes: 115
+-- Total Accepted:    305.4K
+-- Total Submissions: 474.4K
+-- Testcase Example:  '{"headers":{"Accounts":["account_id","income"]},"rows":{"Accounts":[[3,108939],[2,12747],[8,87709],[6,91796]]}}'
+--
+-- Table: Accounts
+-- 
+-- 
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | account_id  | int  |
+-- | income      | int  |
+-- +-------------+------+
+-- account_id is the primary key (column with unique values) for this table.
+-- Each row contains information about the monthly income for one bank
+-- account.
+-- 
+-- 
+-- 
+-- 
+-- Write a solution to calculate the number of bank accounts for each salary
+-- category. The salary categories are:
+-- 
+-- 
+-- "Low Salary": All the salaries strictly less than $20000.
+-- "Average Salary": All the salaries in the inclusive range [$20000,
+-- $50000].
+-- "High Salary": All the salaries strictly greater than $50000.
+-- 
+-- 
+-- The result table must contain all three categories. If there are no accounts
+-- in a category, return 0.
+-- 
+-- Return the result table in any order.
+-- 
+-- The result format is in the following example.
+-- 
+-- 
+-- Example 1:
+-- 
+-- 
+-- Input: 
+-- Accounts table:
+-- +------------+--------+
+-- | account_id | income |
+-- +------------+--------+
+-- | 3          | 108939 |
+-- | 2          | 12747  |
+-- | 8          | 87709  |
+-- | 6          | 91796  |
+-- +------------+--------+
+-- Output: 
+-- +----------------+----------------+
+-- | category       | accounts_count |
+-- +----------------+----------------+
+-- | Low Salary     | 1              |
+-- | Average Salary | 0              |
+-- | High Salary    | 3              |
+-- +----------------+----------------+
+-- Explanation: 
+-- Low Salary: Account 2.
+-- Average Salary: No accounts.
+-- High Salary: Accounts 3, 6, and 8.
+-- 
+-- 
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+
+select 'Low Salary' as category,count(*) as accounts_count
+from accounts
+where income<20000
+union 
+select 'Average Salary' as category,count(*) as accounts_count
+from accounts
+where income between 20000 and 50000
+union 
+select 'High Salary' as category,count(*) as accounts_count
+from accounts
+where income > 50000
+-- @lc code=end
+
